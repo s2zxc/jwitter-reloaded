67dc7e4c4e13684086c2a92f6c21138a1da983f5
s2zxc <s2zxc@hanmail.net>
2024-07-23 15:29:27 +0900

    2024.07.23 올릴내용 저장

--- src/components/post-wteet-form.tsx
+++ src/components/post-wteet-form.tsx
@@ -1,7 +1,8 @@
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, updateDoc } from "firebase/firestore";
 import React, { useState } from "react";
 import styled from "styled-components"
-import { auth, db } from "../routes/firebase";
+import { auth, db, storage } from "../routes/firebase";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
 const Form = styled.form`
     display: flex;
@@ -86,14 +87,26 @@ export default function PostTweetForm() {
         try{
             setLoading(true);
             // firebase SDK의 기능으로 어떤 컬랙션에 document를 생성하고 싶은지 지정
-            await addDoc(collection(db, "tweet"),{
+            const doc = await addDoc(collection(db, "tweet"),{
                 tweet, // 트윗내용
                 createdAt:Date.now(), // 트윗게시 시간
                 userName:user.displayName || "Anonymous", // 유저 이름
                 userId : user.uid, // 유저 아이디(차후 삭제 기능관련해서 일치여부 판단하기 위함)
 
             });
+            if(file){
+                const locationRef = ref( // 글및 사진 게시시 데이터가 저장되는 경로 설정
+                    storage, `tweets/${user.uid}-${user.displayName}/${doc.id}`
+                ); 
+                const result = await uploadBytes(locationRef, file); // 업로드 되는 경로및 파일을 토대로 저장소에 업로드
+                const url = await getDownloadURL(result.ref); // 이미지 다운로드 링크 생성
+                await updateDoc( // 저장소 업데이트 (최종적으로 파이어베이스 데이터베이스와 스토레지 연결)
+                    doc,{
+                    photo : url,
+                });
+            }
             setTweet("");
+            setFile(null);
         }catch(e){
             console.log(e);
         }finally{
@@ -104,6 +117,7 @@ export default function PostTweetForm() {
     return (
         <Form onSubmit={onSubmit}>
             <TextArea
+                required
                 rows={5}
                 maxLength={180}
                 onChange={onChange}
